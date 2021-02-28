d570a890fa5de61cfc7670d6b688a3680fb5dad8
Sarah Souliman <sarah.sliman2778@gmail.com>
2021-02-28 14:27:25 +0200

    updateTheAgencyModelChangingSubTypePropToMtType

---
+++ Migrations/20210228115050_EditAgencyModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MtNews.Migrations
+{
+    public partial class EditAgencyModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SubType",
+                table: "Agencies",
+                newName: "MtType");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MtType",
+                table: "Agencies",
+                newName: "SubType");
+        }
+    }
+}
